ecf7f0d4f4756ce5ef7affe33012d1aeda57b9f8
Okens <Ng.Eric314@gmail.com>
2018-10-05 15:49:30 -0700

    Adding organized test validation.

--- test/test.js
+++ test/test.js
@@ -6,25 +6,36 @@ const createData = test_objects.createData;
 
 const createItem = (createData) => axios.post('https://zruegdeqol.execute-api.us-west-1.amazonaws.com/dev/tasks/create', createData)
   .then(response => {
-    // console.log(response);
-    console.log('-----------------------------------------------');
+    let testResult = false;
     const data = response.data;
 
-    console.log(`createTask function: `);
-    console.log(data);
-    return data;
-  })
+    if (data.taskId === createData.taskId) {
+      testResult = true;
+    }
+
+    return testResult;
+  });
 // createItem(createData);
 
 
 const getAllTasks = () => axios.get('https://zruegdeqol.execute-api.us-west-1.amazonaws.com/dev/tasks/all')
-.then(response => {
-  // console.log(response);
-  const data = response.data;
+  .then(response => {
+    let testResult = false;
+    let testResultOfCreate = false;
+    const data = response.data;
 
-  console.log(`getAllTasks function: `)
-  console.log(JSON.stringify(data))
-})
+    if (data.length > 0){
+      testResult = true;
+    }
+
+    for (let i = 0; i < data.length; i++) {
+      if(data[i].taskId === createData.taskId){
+        testResultOfCreate = true;
+      }
+    }
+
+    return {'testResult': testResult, 'testResultOfCreate': testResultOfCreate};
+  });
 // getAllTasks()
 
 // axios.patch('https://zruegdeqol.execute-api.us-west-1.amazonaws.com/dev/tasks/update/{taskId}')
@@ -45,3 +56,28 @@ const getAllTasks = () => axios.get('https://zruegdeqol.execute-api.us-west-1.am
 //   console.log(`deleteTask function: `)
 //   console.log(JSON.stringify(data))
 // })
+
+async function test() {
+  let testComplete = false;
+  console.log('-----------------START TEST-------------------')
+  try {
+    console.log('-----------------createItem TEST-------------------')
+    const createItemTest = await createItem(createData);
+    console.log(`${createItemTest} - createItem - TEST RESULT`);
+
+    console.log('-----------------getAllTasks TEST-------------------')
+    const getAllTaskTest = await getAllTasks();
+    console.log(`${getAllTaskTest.testResult} - getAllTaskTest - ALL ITEMS TEST RESULT`);
+    console.log(`${getAllTaskTest.testResultOfCreate} - getAllTaskTest - createItem VALIDATE TEST RESULT:`);
+
+    testComplete = true;
+  } catch (err) {
+
+    console.log('-----------------ERROR-------------------')
+    console.log(err);
+    testComplete = false;
+  }
+  console.log('-----------------END TEST-------------------')
+  console.log(`TEST COMPLETE: ${testComplete}`);
+}
+test();
