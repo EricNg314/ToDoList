a9ec7232115425fae2ce3216d11704ae5d1995fb
Okens <Ng.Eric314@gmail.com>
2018-10-05 20:33:26 -0700

    Adding getTasksByStatus tests to validate function in api.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,10 @@ const axios = require('axios');
 
 const test_objects = require('./test_objects.js');
 const createData = test_objects.createData;
+
+// NOTE: Updating the updateData's status will change results of getTasksByStatus. Consistent validation of testing.
 const updateData = test_objects.updateData;
+const statusQueryValue = updateData.status;
 
 const createItem = (createData) => axios.post('https://zruegdeqol.execute-api.us-west-1.amazonaws.com/dev/tasks/create', createData)
   .then(response => {
@@ -41,6 +44,30 @@ const getAllTasks = () => axios.get('https://zruegdeqol.execute-api.us-west-1.am
     return { 'testResult': testResult, 'testResultOfCreate': testResultOfCreate };
   });
 // getAllTasks()
+
+
+const getTasksByStatus = (createData, status) => axios.get(`https://zruegdeqol.execute-api.us-west-1.amazonaws.com/dev/tasks/filter?status=${status}`)
+  .then(response => {
+    let testResult = false;
+    let testResultOfUpdate = false;
+
+    // console.log(response);
+    const data = response.data;
+
+    if (response.status === 200) {
+      testResult = true;
+    }
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].taskId === createData.taskId && data[i].status === status) {
+        testResultOfUpdate = true;
+      }
+    }
+
+    return { 'testResult': testResult, 'testResultOfUpdate': testResultOfUpdate };
+  })
+  // getTasksByStatus(createData, statusQueryValue);
+
 
 const updateTask = (taskId, updateData) => axios.patch(`https://zruegdeqol.execute-api.us-west-1.amazonaws.com/dev/tasks/update/${taskId}`, updateData)
   .then(response => {
@@ -88,15 +115,25 @@ async function test(createData, updateData) {
     console.log(`TEST RESULT - ${getAllTaskTest.testResult} - getAllTask`);
     console.log(`TEST RESULT - ${getAllTaskTest.testResultOfCreate} - getAllTask - createItem VALIDATION \n`);
 
+    console.log('-----------------getTasksByStatus IF "Complete" TEST-------------------')
+    const getTasksByStatusTest = await getTasksByStatus(createData, statusQueryValue);
+    console.log(`TEST RESULT - ${getTasksByStatusTest.testResult} - getTasksByStatus`);
+    console.log(`TEST RESULT - ${!getTasksByStatusTest.testResultOfUpdate} - getTasksByStatus - updateTask VALIDATION \n`);
+
     console.log('-----------------updateTask TEST-------------------')
     const updateTaskTest = await updateTask(createData.taskId, updateData);
     console.log(`TEST RESULT - ${updateTaskTest} - updateTask\n`);
+
+    console.log('-----------------getTasksByStatus IF "Complete" AFTER updateTask TEST-------------------')
+    const getTasksByStatusAfterUpdateTest = await getTasksByStatus(createData, statusQueryValue);
+    console.log(`TEST RESULT - ${getTasksByStatusAfterUpdateTest.testResult} - getTasksByStatus`);
+    console.log(`TEST RESULT - ${getTasksByStatusAfterUpdateTest.testResultOfUpdate} - getTasksByStatus - updateTask VALIDATION \n`);
 
     console.log('-----------------deleteTask TEST-------------------')
     const deleteTaskTest = await deleteTask(createData.taskId);
     console.log(`TEST RESULT - ${deleteTaskTest} - deleteTask\n`);
 
-    console.log('-----------------getAllAfterDelete TEST-------------------')
+    console.log('-----------------getAll AFTER deleteTask TEST-------------------')
     const getAllAfterDeleteTest = await getAllTasks(updateData);
     console.log(`TEST RESULT - ${getAllAfterDeleteTest.testResult} - getAllTasks`);
     console.log(`TEST RESULT - ${!getAllAfterDeleteTest.testResultOfCreate} - getAllTasks - deleteTask VALIDATION \n`);
@@ -111,4 +148,4 @@ async function test(createData, updateData) {
   console.log('-----------------END TEST-------------------')
   console.log(`TEST COMPLETE: ${testComplete}\n`);
 }
-test(createData, updateData);
+test(createData, updateData, statusQueryValue);
